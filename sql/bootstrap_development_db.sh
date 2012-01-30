94443514f858ec02c84fc0a6099f7e7f3ab253f1
Laurent Bossavit <laurent.git@bossavit.com>
2012-01-30 14:51:16 +0100

    Create DB in UTF8

--- sql/bootstrap_development_db.sh
+++ sql/bootstrap_development_db.sh
@@ -4,7 +4,7 @@ dbs="reddit changes email query_queue reddit_test changes_test email_test query_
 
 for db in $dbs; do
     sudo -u postgres dropdb $db
-    sudo -u postgres createdb -E utf8 $db
+    sudo -u postgres createdb -E utf8 $db --lc-ctype 'en_US.utf8' --lc-collate 'en_US.utf8' --template template0
 done
 
 psql -U reddit -W < sql/functions.sql
